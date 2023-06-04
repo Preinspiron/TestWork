33772f15ff0c587c8b4ff50a4fe943c9f92a0d1e
V_Borysenko <preinspiron@gmail.com>
2023-06-04 13:23:36 +0200

    base

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import Routing from './routes/Routing';
-import './App.css   ';
+import './App.css';
 
 function App() {
   return <Routing />;
