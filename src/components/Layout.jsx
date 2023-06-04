8f10c83436ddaf4615d5a50467525fccd8faacc9
V_Borysenko <preinspiron@gmail.com>
2023-06-04 15:31:18 +0200

    123

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,21 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import s from './Layout.module.scss';
 import { Container } from '@mui/material';
 const Layout = () => {
+  const location = useLocation();
   return (
     <>
       <Container maxWidth="1280px">
+        r
         <nav className={s.nav}>
           <Link to="/">
             <Button variant="outlined">HOME</Button>
           </Link>
           <Link to="/tweets">
-            <Button variant="outlined">TWEETS</Button>
+            <Button variant="outlined" state={{ from: location }}>
+              TWEETS
+            </Button>
           </Link>
         </nav>
         <Outlet />
